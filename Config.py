eab07b8cc04a55376be3d76b613e44a70795271a
Mugen Rao <84302496+Master-JeyDee@users.noreply.github.com>
2021-05-23 11:33:35 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -2,10 +2,10 @@ import os
 
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", 1095886))  # Change 12345 to your API_ID
-    API_HASH = os.environ.get("API_HASH", "f8bd562b2e0d5de5b55b294ed4405566")  # Change None to your API_HASH
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "1811559815:AAFGyDSbOK5qi2beZHtlKEE33COLeGuMi8U")  # Change None to your BOT_TOKEN
-    OWNER_ID = int(os.environ.get("OWNER_ID", 830872779))  # Change 0 to your OWNER_ID
+    API_ID = int(os.environ.get("API_ID", 2965795))  # Change 12345 to your API_ID
+    API_HASH = os.environ.get("API_HASH", "4ab39444fbfe934773a2e344108c2cd3")  # Change None to your API_HASH
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "1861170132:AAFQr9JoRMHGXO1CfAeyHf74KmgRUIP5M0k")  # Change None to your BOT_TOKEN
+    OWNER_ID = int(os.environ.get("OWNER_ID", 1048846990))  # Change 0 to your OWNER_ID
     OWNER_NAME = os.environ.get("OWNER_NAME", "MugenRao")  # Change None to your OWNER_NAME
 
     # For Local Deploys edit above 5 lines.
